9139bb5947cce32392e22927be3900c212f9fbcd
SIDDHARTH SINGH <musicsiddharth@gmail.com>
2019-05-29 07:10:08 +0530

    Add files via upload

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Meeting Planner Application",
+  "version": "0.0.1",
+  "description": "Documentation for the Meeting Planner App",
+  "title": "Meeting Planner APIs",
+  "url": "http://localhost:3002",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-27T21:10:51.014Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
